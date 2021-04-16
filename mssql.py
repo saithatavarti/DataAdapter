70f97f524131a3869a246762d57c24e68ba2819c
ChillaleSaikrishna <chillale.saikrishna@gmail.com>
2021-04-16 11:31:06 +0530

    updated pagination

--- mssql.py
+++ mssql.py
@@ -1,3 +1,5 @@
+# microsoft sql
+
 from fastapi import FastAPI
 import pyodbc
 import sqlalchemy as sal
@@ -13,21 +15,56 @@ def index():
     return {'data':{'name':'krishna'}}
 
 
-@app.get("/items/{choose_id}")
-def test(choose_id):
+@app.get("/{choose_id}/{min}/{offset}")
+def test(choose_id,min,offset):
+    url = choose_id
+    lower=int(min)
+    upper=int(offset)
+    min=int(min)-1
+    max=int(offset)-int(min)
+    page = int(upper-lower+1)
+
     with open(choose_id+".yaml") as file:
         documents = yaml.safe_load(file)
     adapter = str(documents["parameters"]["adapter"])
     username = str(documents["parameters"]["username"])
     driver = str(documents["parameters"]["drivers"])
     db = documents["parameters"]["database"]
+    address = str(documents["parameters"]["address"])
+    query=str(documents["query"])
+    print(query)
+    query2=query.replace("*","top {} * ".format(min))
+    print(query2)
+    query3=query.replace("*","top {} * ".format(offset))
+    print(query3)
+    finalquery="{}  EXCEPT {}".format(query3,query2)
+    print(finalquery)
     link =""
     link = adapter+"://"+username+"/"+db+"?driver="+driver+"?Trusted_Connection=yes"
-    engine = sal.create_engine(link)
-    conn = engine.connect()
-    result = conn.execute("select * from details").fetchall()
-    return(result)
 
     
+    engine = sal.create_engine(link)
+    conn = engine.connect()
+    lst = conn.execute(finalquery).fetchall()
+    lst1 = len(conn.execute(query).fetchall())
+    f = int(lst1)
+    if(f%int(page)==0):
+        total_pages=int(f/page)
+    else:
+        total_pages=int((f)/page)+1
+
+    str1="next_url== "+address+url+"/"+str(lower+page)+"/"+str(upper+page)
+    print(min,max)
+    data= "data: "
+    meta= "meta: "
+
+
+    if((upper/page)>total_pages):
+        return("end of pages")
+    else:
+        return data,lst,{meta , total_pages,str1}
+
+    
+
     
     
