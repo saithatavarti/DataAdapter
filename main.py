f8990bd8f0c0a2d199603c20a96b6e0f0b09b64b
ChillaleSaikrishna <chillale.saikrishna@gmail.com>
2021-04-16 11:29:13 +0530

    updated pagination and few function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# postgresql 
+
 import yaml
 from fastapi import FastAPI
 import json
@@ -11,10 +13,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
-#  dynamic method for opting a yaml file 
-
-@app.get("/postgresql/{choose_id}/{username}/{minimum}/{offset}")
+@app.get("/{choose_id}/{username}/{minimum}/{offset}")
 async def get_component(choose_id,username,minimum,offset):
 	url = choose_id
 	us = username
@@ -22,7 +21,8 @@ async def get_component(choose_id,username,minimum,offset):
 	upper= int(offset)
 	min = int(offset)-int(minimum)+1
 	max = int(minimum)-1
-
+	page = int(upper-lower+1)
+	
 	with open(url+".yaml") as file:
 		documents = yaml.safe_load(file)
 	adapter = str(documents["parameters"]["adapter"])    
@@ -30,110 +30,44 @@ async def get_component(choose_id,username,minimum,offset):
 	password = str(documents["parameters"]["password"])
 	db = documents["parameters"]["database"]
 	port = str(documents["parameters"]["database_port"])
-
-	# query
-	columns = str(documents["query"]["column"])
-	table = str(documents["query"]["table"])
-	limit = str(documents["query"]["limit"])
-	where = str(documents["query"]["where"])
-	order = str(documents["query"]["order"])
-	group = str(documents["query"]["group"])
-	page =  int(documents["query"]["page_size"])
-
-	# apended variable
+	address = str(documents["parameters"]["address"])
+	query = str(documents["query"]["code"])
 	link =""
 	link = adapter+"://"+username+":"+password+"@localhost:"+port+"/"+db
-	# query = "select"+" "+column+" "+"from"+" "+table +" " +"LIMIT"+ " " +limit
-	query="select "	
-	if (columns=="None"):
-		return("column names incorrect")
-	else:
-		query=query+columns+" "
-	if (table=="None"):
-		return("table doesn't exist")
-	else:
-		query=query+"from"+" "+table
-	if (where== "None"):
-		query=query
-	else:
-		query=query+" "+"where"+" "+where
-	if(group=="None"):
-		query=query
-	else:
-		query=query+" "+"group by"+" "+group
-	if(order=="None"):
-		query=query
-	else:
-		query=query+" "+"order by"+" "+order
-	
-	query1="select "	
-	if (columns=="None"):
-		return("column names incorrect")
-	else:
-		query1=query1+"count("+columns+") "
-	if (table=="None"):
-		return("table doesn't exist")
-	else:
-		query1=query1+"from"+" "+table
-	if (where== "None"):
-		query1=query1
-	else:
-		query1=query1+" "+"where"+" "+where
-	if(group=="None"):
-		query1=query1
-	else:
-		query1=query1+" "+"group by"+" "+group
-	if(order=="None"):
-		query1=query1
-	else:
-		query1=query1+" "+"order by"+" "+order
-	print(query1)
 
-
-	if(limit=="None"):
-		max=max
-	elif(int(limit)<=int(offset)):
-		offset=int(limit)
-		min = int(offset)-int(minimum)+1
-		max = int(minimum)-1
-	else:
-		max=max
 	if(max<0 or min<1):
 		return("limit is given wrong")
 	else:
-		query=query+" "+"limit"+" "+str(min)+" offset "+str(max)
-
-
+		query1=query+" "+"limit"+" "+str(min)+" offset "+str(max)
 
 	# db connection 
 	
-		engine = create_engine(link,echo=False)
-		connection=engine.connect()
-		userQuery = "select * from username where name = '{0}' ".format(us)
-		print (userQuery)
-		usercheck = connection.execute(userQuery).fetchall()
-		print(usercheck)
-
+	engine = create_engine(link,echo=False)
+	connection=engine.connect()
+	userQuery = "select * from username where name = '{0}' ".format(us)
+	print (userQuery)
+	usercheck = connection.execute(userQuery).fetchall()
+	print(usercheck)
 
 	if usercheck:
-		lst = connection.execute(query).fetchall()
-		lst1 = connection.execute(query1).fetchall()
-		f=int(lst1[0][0])
+		
+		lst = connection.execute(query1).fetchall()
+		count= len(connection.execute(query).fetchall())
+		f=int(count)
 		if(f%int(page)==0):
 			total_pages=int(f/page)
 		else:
 			total_pages=int((f)/page)+1
 		
-		str1="next_url== http://127.0.0.1:8000/postgresql/"+url+"/"+us+"/"+str(lower+page)+"/"+str(upper+page)
+		str1="next_url== "+address+url+"/"+us+"/"+str(lower+page)+"/"+str(upper+page)
 		print(min,max)
+		data= "data: "
+		meta= "meta: "
 
-
-		if((upper/10)>total_pages):
+		if((upper/page)>total_pages):
 			return("end of pages")
 		else:
-			return lst,total_pages,str1
-				# print(lst)
-				# return(lst)
+			return data,lst,{meta ,total_pages,str1}
 	else:
 		return "User Does not Exists"
    
