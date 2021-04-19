2a3e3eecd149a5d6a17f8d7ef3bc200c713f331a
shabbirhussain09 <79976046+shabbirhussain09@users.noreply.github.com>
2021-04-19 15:00:11 +0530

    sqlite data adapter with user authentication

--- db_creation.py
+++ db_creation.py
@@ -4,6 +4,10 @@ import sqlite3
 con = sqlite3.connect("medusa.db") 
 print("Database opened successfully")
 
-#Creating table
+#Creating data table
 con.execute("create table students_sqlite (id INTEGER PRIMARY KEY AUTOINCREMENT,email TEXT NOT NULL, name TEXT NOT NULL)")
+print("Table created succesfully")
+
+#Creating Users table
+con.execute("create table users_dataadapter (name TEXT NOT NULL)")
 print("Table created succesfully")
